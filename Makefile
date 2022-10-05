bf0ef94b2185c8c0c1650cbeb71b251ec3a54fe1
Laure-di <62625835+Laure-di@users.noreply.github.com>
2022-10-05 12:59:47 +0200

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,23 @@
+NAME = inception
+
+WP_VOL = /home/lmasson/data/wp_vol/
+
+DB_VOL = /home/lmasson/data/db_vol/
+
+all:
+	sudo mkdir -p $(WP_VOL)
+	sudo mkdir -p $(DB_VOL)
+	docker-compose -f srcs/docker-compose.yml build
+	docker-compose -f srcs/docker-compose.yml up 2>1 > error.log
+up:
+	docker-compose -f ./srcs/docker-compose.yml up -d
+
+down:
+	docker-compose -f ./srcs/docker-compose.yml down
+
+clean: down
+	docker system prune -a;
+
+fclean: clean
+		sudo rm -rf $(WP_VOL)
+		sudo rm -rf $(DB_VOL)
---
+++ srcs/requirements/mariadb/setup_script.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+mysql_install_db
+service mysql start
+
+if [ ! -d /var/lib/mysql/wp_wordpress ]; then
+mysql_secure_installation<<EOF
+
+y
+secret
+secret
+y
+y
+y
+y
+EOF
+
+until mysqladmin ping;do
+sleep 2
+done
+mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
+mysql -u root -e "CREATE USER '${MYSQL_ADMIN}'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN}'@'localhost';"
+mysql -u root -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT ALL ON wp_wordpress.* TO '${MYSQL_USER}'@'localhost'"
+mysql -u root -e "FLUSH PRIVILEGES;"
+echo "Database created!"
+else
+echo "The database already exist!"
+fi
+#service mysql stop
