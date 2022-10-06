3f3290500cfd6e658ca568b05dbdd48df32339ed
Laure-Di <masson.laure.l@gmail.com>
2022-10-06 15:24:24 +0200

    dockerfile change

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -7,7 +7,8 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 	wp config create --dbname=wordpress --dbuser=${ADMIN} --dbpass=${ADMIN_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 	wp core install --url=${WP_URL} --title="${WP_TITLE}" --admin_user=${ADMIN} --admin_password=${ADMIN_PASSWD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root
 	wp user create ${USER_WP} ${USER_EMAIL_WP} --role=author --user_pass=${USER_WP_PASS} --allow-root
-	wp theme install "teluro" --activate --allow-root
+	wp theme install twentysixteen --activate -allow-root
+#wp theme install "teluro" --activate --allow-root
 else
 		echo "wp already exist"
 fi
