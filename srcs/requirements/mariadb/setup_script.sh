880e0d427be845baec08326c4ad2e219b9959c77
Laure-Di <masson.laure.l@gmail.com>
2022-10-06 16:55:37 +0200

    dockerfile change mariadb sh

--- srcs/requirements/mariadb/setup_script.sh
+++ srcs/requirements/mariadb/setup_script.sh
@@ -6,7 +6,7 @@ if [ ! -d /var/lib/mysql/wordpress ]; then
 		sleep 2
 	done
 	echo "create database if not exists wordpress;" | mysql -u root
-	echo "CREATE USER IF NOT EXISTS '$ADMIN'@'%' IDENTIFIED BY '$ADMIN_PASSWORD';" | mysql -u root
+	echo "CREATE USER IF NOT EXISTS '$ADMIN' IDENTIFIED BY '$ADMIN_PASSWORD';" | mysql -u root
 	echo "GRANT USAGE ON wordpress.* TO '$ADMIN'@'%' IDENTIFIED BY '$ADMIN_PASSWORD';" | mysql -u root
 	echo "FLUSH PRIVILEGES;" | mysql -u root
 	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SUPER_PWD';"| mysql -u root 
