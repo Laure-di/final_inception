04a881671df0db01a85d8d1bf7b2a54e491a11c0
Laure-Di <masson.laure.l@gmail.com>
2022-10-06 17:14:20 +0200

    dockerfile change mariadb sh

--- srcs/requirements/mariadb/setup_script.sh
+++ srcs/requirements/mariadb/setup_script.sh
@@ -5,12 +5,12 @@ if [ ! -d /var/lib/mysql/wordpress ]; then
 		echo "coucou"
 		sleep 2
 	done
-	echo "create database if not exists wordpress;" | mysql -u root
-	echo "CREATE USER IF NOT EXISTS '$ADMIN' IDENTIFIED BY '$ADMIN_PASSWORD';" | mysql -u root
-	echo "GRANT USAGE ON wordpress.* TO '$ADMIN'@'%' IDENTIFIED BY '$ADMIN_PASSWORD';" | mysql -u root
-	echo "GRANT ALL PRIVILEGES ON wordpress.* TO '$ADMIN' IDENTIFIED BY '$ADMIN_PASSWORD';" | mysql -u root 
-	echo "FLUSH PRIVILEGES;" | mysql -u root
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SUPER_PWD';"| mysql -u root 
+	mysql -u root -e "create database if not exists wordpress;" #| mysql -u root
+	mysql -u root -e "CREATE USER IF NOT EXISTS '$ADMIN' IDENTIFIED BY '$ADMIN_PASSWORD';" #| mysql -u root
+	mysql -u root -e "GRANT USAGE ON wordpress.* TO '$ADMIN'@'%' IDENTIFIED BY '$ADMIN_PASSWORD';" #| mysql -u root
+	mysql -u root -e  "GRANT ALL PRIVILEGES ON wordpress.* TO '$ADMIN' IDENTIFIED BY '$ADMIN_PASSWORD';" #| mysql -u root 
+	mysql -u root -e "FLUSH PRIVILEGES;" #| mysql -u root
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SUPER_PWD';"#| mysql -u root 
 	echo "Database created!"
 	killall mysqld
 else
