5d78762631acc46cf6b1aa9c7015c4a1ffdc337e
Laure-Di <masson.laure.l@gmail.com>
2022-10-06 14:38:41 +0200

    is the vm the isssue

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -4,6 +4,6 @@
         sleep 2
     done
 	wp config create --dbname=wordpress --dbuser=${ADMIN} --dbpass=${ADMIN_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --url=${URL}/wordpress --title="${TITLE}" --admin_user=${ADMIN} --admin_password=${ADMIN_PASSWD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root
+	wp core install --url=${WP_URL}/wordpress --title="${WP_TITLE}" --admin_user=${ADMIN} --admin_password=${ADMIN_PASSWD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root
 	wp user create ${USER_WP} ${USER_EMAIL_WP} --role=author --user_pass=${USER_WP_PASS} --allow-root
 exec "$@"
