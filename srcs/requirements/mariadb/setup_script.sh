bc6225bd7a3dc3883161ca1054b96d5766cee3ed
Laure-Di <masson.laure.l@gmail.com>
2022-10-06 16:16:22 +0200

    dockerfile change mariadb sh

--- srcs/requirements/mariadb/setup_script.sh
+++ srcs/requirements/mariadb/setup_script.sh
@@ -5,11 +5,11 @@ if [ ! -d /var/lib/mysql/wordpress ]; then
 		echo "coucou"
 		sleep 2
 	done
-	mysql -u root "CREATE DATABASE IF NOT EXISTS wordpress;"
-	mysql -u root "CREATE USER IF NOT EXISTS '${ADMIN}'@'%' IDENTIFIED BY '${ADMIN_PASSWORD}';"
-	mysql -u root "GRANT USAGE ON wordpress.* TO '${ADMIN}'@'%' WITH GRANT OPTION;"
-	mysql -u root "FLUSH PRIVILEGES;"
-	mysql -u root "ALTER USER 'root'@'localhost' IDENTIFIED BY '${UPER_PWD}'"
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+	mysql -u root -e "CREATE USER IF NOT EXISTS '${ADMIN}'@'%' IDENTIFIED BY '${ADMIN_PASSWORD}';"
+	mysql -u root -e "GRANT USAGE ON wordpress.* TO '${ADMIN}'@'%' WITH GRANT OPTION;"
+	mysql -u root -e "FLUSH PRIVILEGES;"
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${UPER_PWD}'"
 	echo "Database created!"
 	killall mysqld
 else
