1850a54dc2c2547f3bceccbc01efcf65adcf22b6
Laure-Di <masson.laure.l@gmail.com>
2022-10-06 14:54:47 +0200

    security wp script

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core download --allow-root
 	until mysqladmin -hmariadb -u${ADMIN} -p${ADMIN_PASSWORD} ping; do
         sleep 2
@@ -6,4 +7,6 @@
 	wp config create --dbname=wordpress --dbuser=${ADMIN} --dbpass=${ADMIN_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 	wp core install --url=${WP_URL}/wordpress --title="${WP_TITLE}" --admin_user=${ADMIN} --admin_password=${ADMIN_PASSWD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root
 	wp user create ${USER_WP} ${USER_EMAIL_WP} --role=author --user_pass=${USER_WP_PASS} --allow-root
+	else
+	echo "wp already exist"
 exec "$@"
