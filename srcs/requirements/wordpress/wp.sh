2a9b4fb0ed05df27996bfe5cb8fb1c133a167d00
Laure MASSON <lmasson@e1r1p3.clusters.42paris.fr>
2022-10-06 05:48:15 +0200

    last chance

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -4,9 +4,9 @@ echo "wp already here"
 else
  	cd /var/www/html/
 	wp core download --allow-root
-	wp config create --dbname=${MYSQL_DB} --dbuser=${MYSQL_ADMIN} --dbpass=${MYSQL_ADMIN_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --url=${URL} --title="${TITLE}" --admin_user=${ADMIN_WP} --admin_password=${ADMIN_PASSWD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root
-	wp user create ${USER_WP} ${USER_MAIL_WP} --role=author --user_pass=${USER_WP_PASS} --allow-root
+	wp config create --dbname=wordpress --dbuser=${ADMIN} --dbpass=${ADMIN_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	wp core install --url=${URL} --title="${TITLE}" --admin_user=${ADMIN} --admin_password=${ADMIN_PASSWD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root
+	wp user create ${USER_WP} ${USER_EMAIL_WP} --role=author --user_pass=${USER_WP_PASS} --allow-root
 	wp theme install "teluro" --activate --allow-root
 fi
 
